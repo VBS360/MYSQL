139c848e874d4899b8d31edc99e1e397b334894f
Vatsal Shah <96993029+VBS360@users.noreply.github.com>
2024-09-15 12:34:47 +0530

    Add files via upload

---
+++ Day 5/Table/Creating a Table.sql
@@ -0,0 +1,23 @@
+create database sales;
+use sales;
+
+create table sales_data
+(
+	purchase_no int primary key auto_increment,
+	date_of_purchase date not null,
+	customer_id int,
+	item_code varchar(10) not null
+);
+
+select * from sales_data;
+
+create table customer
+(
+	customer_id int primary key,
+    first_name varchar(255) not null,
+    last_name varchar(255),
+    email_address varchar(255),
+    number_of_complaints int
+);
+
+select * from customer;
